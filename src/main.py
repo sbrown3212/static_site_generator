3e2f34a7e869fa26d0940f9f39a2808a680e71a8
Stephen Brown <46618639+sbrown3212@users.noreply.github.com>
2025-01-24 14:09:59 -0700

    imported textnode module, defined main to create textnode object and print node

---
+++ src/main.py
@@ -0,0 +1,8 @@
+from textnode import *
+
+def main():
+    text_node = TextNode("This is a text node", TextType.BOLD, "https://boot.dev")
+    print(text_node)
+
+if __name__ == "__main__":
+    main()
