d0c10eca34ae229041954126e3b9d0b18138851e
Stephen Brown <46618639+sbrown3212@users.noreply.github.com>
2025-01-26 16:04:11 -0700

    refactored to move leafnode class definition into htmlnode.py, and same for leafnode tests to test_htmlnode.py

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,9 +1,10 @@
 import unittest
 
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
 
 class TestHTMLNode(unittest.TestCase):
+    # HTMLNode tests
     def test_props_to_html(self):
         node_props = {"href": "https://www.google.com", "target": "_blank",}
         node = HTMLNode(props=node_props)
@@ -27,6 +28,25 @@ class TestHTMLNode(unittest.TestCase):
     def test_repr2(self):
         node = HTMLNode(value="this is a test html node")
         self.assertEqual('HTMLNode(None, this is a test html node, None, None)', repr(node))
+
+    # LeafNode tests
+    def test_to_html(self):
+        node = LeafNode("p", "This is a paragraph of text.")
+        self.assertEqual("<p>This is a paragraph of text.</p>", node.to_html())
+
+    def test_to_html2(self):
+        node = LeafNode("a", "Click me!", {"href": "https://www.google.com"})
+        self.assertEqual(node.to_html(), '<a href="https://www.google.com">Click me!</a>')
+    
+    def test_props_multiple(self): # From Boots
+        node = LeafNode("p", "Hello", {"class": "greeting", "id": "hello"})
+        # print(f"Node props: {node.props}")  # Debug print
+        result = node.props_to_html()
+        # print(f"Actual result: '{result}'")
+        self.assertTrue(
+            result == ' class="greeting" id="hello"' or 
+            result == ' id="hello" class="greeting"'
+        )
 
 
 if __name__ == "__main__":
