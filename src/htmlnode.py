8920fe5da61013223a2dbaee21ce3d7a92822eac
Stephen Brown <46618639+sbrown3212@users.noreply.github.com>
2025-02-07 11:55:21 -0800

    added eq dunder method to HTMLNode

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -19,6 +19,14 @@ class HTMLNode():
 
         return "".join(map(convert_item_to_html, self.props.items()))
     
+    def __eq__(self, other):
+        return (
+            self.tag == other.tag and
+            self.value == other. value and
+            self.children == other.children and
+            self.props == other.props
+        )
+    
     def __repr__(self):
         return f"HTMLNode({self.tag}, {self.value}, {self.children}, {self.props})"
     
